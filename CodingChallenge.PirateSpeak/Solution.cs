6a142849f0e6e8c1618345522ec9c0197cad5201
Caitlin Murnick <cmurnick@benefitcommunications.com>
2018-04-21 11:24:34 -0500

    please work

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,13 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodingChallenge.PirateSpeak
 {
     public class Solution
     {
+
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            //I think that I need to break out the string jumble and parse it to single letters
+            // then loop through the single letters and send to a list
+            // then make a dictionary
+
+            //string str = jumble;
+            //char[] charArray = str.ToCharArray();
+            //string[] strArray = str.Select(x => x.ToString()).ToArray();
+
+            string message = jumble;
+            string[] result = new string[message.Length];
+            char[] letters = new char[message.Length];
+
+            letters = message.ToCharArray();
+
+            List<string> listLetters = new List<string>();
+            var please = "";
+            for (int i = 0; i < message.Length - 1; i++)
+            {
+                result[i] = Convert.ToString(letters[i]);
+
+                if (dictionary.Contains(result[i]))
+                {
+                    result[i] = Convert.ToString(letters[i]);
+                    listLetters.Add(result[i]);
+                    please = result[i];
+                }
+            }
+            return dictionary;
         }
     }
 }
+            
+        
